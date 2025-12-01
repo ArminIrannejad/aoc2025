49fe890616b8a3d52285085c8ceb89f7c92dc7d0
ArminIrannejad <armin.irannejad@gmail.com>
2025-12-01 16:36:20 +0100

    chore: solution part 2 day 1

--- src/day01/day01.hs
+++ src/day01/day01.hs
@@ -22,9 +22,8 @@ positions start rs = go start rs []
   where
     go _ [] acc = acc
     go curr (r : rest) acc =
-      -- will be reverse order but shouldn't matter
       let new = moveDial curr r
-          acc' = new : acc
+          acc' = new : acc -- will be reversed but shouldn't matter
        in go new rest acc'
 
 part1 :: Input -> Int
@@ -33,12 +32,39 @@ part1 cont =
       posList = positions 50 instructions
    in length (filter (== 0) posList)
 
+hitsFrom :: Int -> Int -> Int
+hitsFrom distanceToZero steps
+  | distanceToZero > steps = 0
+  | otherwise = 1 + (steps - distanceToZero) `div` 100
+
+hitsDuring :: Int -> Rotation -> Int
+hitsDuring curr (dir, steps) =
+  case dir of
+    'R' ->
+      let base = (100 - curr) `mod` 100
+          distanceToZero = if base == 0 then 100 else base
+       in hitsFrom distanceToZero steps
+    'L' ->
+      let base = curr `mod` 100
+          distanceToZero = if base == 0 then 100 else base
+       in hitsFrom distanceToZero steps
+    _ -> error ""
+
+countAll :: Int -> [Rotation] -> Int -> Int
+countAll _ [] total = total
+countAll curr (r : rest) total =
+  let hitsDuringRotation = hitsDuring curr r
+      endPos = moveDial curr r
+      newTotal = total + hitsDuringRotation
+   in countAll endPos rest newTotal
+
 part2 :: Input -> Int
-part2 _ = error "TODO"
+part2 cont =
+  let rotations = map parseRotation cont
+   in countAll 50 rotations 0
 
 main :: IO ()
 main = do
   cont <- lines <$> readFile "input.txt"
   printf "Part 1: %d\n" $ part1 cont
-
--- printf "Part 2: %d\n" $ part2 cont
+  printf "Part 2: %d\n" $ part2 cont
