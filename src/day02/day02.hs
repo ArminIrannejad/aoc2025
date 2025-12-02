6a6588a72e3574132f5b6fe55e3703cec077f2c2
ArminIrannejad <armin.irannejad@gmail.com>
2025-12-02 12:34:15 +0100

    chore: solution part 1 day 2

--- src/day02/day02.hs
+++ src/day02/day02.hs
@@ -1,20 +1,41 @@
 module Main where
 
-import Text.Printf
 import Data.List.Split
+import Text.Printf
 
 type Input = [String]
 
-parseInput :: String -> (Int, Int)
+type Range = (Int, Int)
+
+parseInput :: String -> Range
 parseInput s =
   let (a, _ : b) = break (== '-') s
    in (read a, read b)
+
+listGenerator :: Range -> [Int]
+listGenerator (a, b) = [a .. b]
+
+isInvalid :: Int -> Bool
+isInvalid n =
+  let s = show n
+      len = length s
+   in even len
+        && let (a, b) = splitAt (len `div` 2) s
+            in a == b
+
+part1 :: Input -> Int
+part1 cont =
+  let ranges = map parseInput cont
+      allIds = concatMap listGenerator ranges
+      invalidIds = filter isInvalid allIds
+   in sum invalidIds
 
 main :: IO ()
 main = do
   raw <- readFile "input.txt"
   let cont = splitOn "," raw
-  print $ map parseInput cont
+  printf "Part 1: %d\n" $ part1 cont
+  -- printf "Part 2: %d\n" $ part2 cont
 
--- printf "Part 1: %d\n" $ part1 cont
--- printf "Part 2: %d\n" $ part2 cont
+-- print $ map listGenerator $ map parseInput cont
+-- print $ map (listGenerator . parseInput) cont
