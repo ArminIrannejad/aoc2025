efedb21a812dba883d14eeae4754e03a1d3d434d
ArminIrannejad <armin.irannejad@gmail.com>
2025-12-03 08:58:51 +0100

    chore: solution day3 part 2

--- src/day03/day03.hs
+++ src/day03/day03.hs
@@ -19,8 +19,38 @@ maxJolt bank =
 part1 :: Input -> Int
 part1 cont = sum $ map maxJolt cont
 
+bestIndex :: Int -> String -> (Int, Char)
+bestIndex _ [] = error ""
+bestIndex allowedFirstsLen s@(c0 : _) = go 0 (0, c0)
+  where
+    go i best@(bestIdx, bestChar)
+      | i >= allowedFirstsLen = best
+      | otherwise =
+          let c = s !! i
+           in if c > bestChar
+                then go (i + 1) (i, c)
+                else go (i + 1) best
+
+bestKthNumber :: Int -> String -> String
+bestKthNumber k s
+  | k <= 0 = "" -- for totalyti
+  | length s <= k = s -- for totality otherwise == should be fine
+  | otherwise =
+      let allowedFirstsLen = length s - k + 1
+          (bestIdx, bestChar) = bestIndex allowedFirstsLen s
+       in bestChar : bestKthNumber (k - 1) (drop (bestIdx + 1) s)
+
+maxJolt12 :: String -> Integer
+maxJolt12 bank =
+  let picked = bestKthNumber 12 bank
+   in read picked
+
+part2 :: Input -> Integer
+part2 cont = sum $ map maxJolt12 cont
+
 main :: IO ()
 main = do
   cont <- lines <$> readFile "input.txt"
   -- print $ map (map digitToInt) cont
   printf "Part 1: %d\n" $ part1 cont
+  printf "Part 2: %d\n" $ part2 cont
--- src/day03/solution.md
+++ src/day03/solution.md
@@ -3,5 +3,15 @@
 ## Expected Result:
 
 ```console
+❯ aoc2025/src/day03 ghc day03.hs -O -o day03 && time ./day03                                                                    on  master [!] via   9.12.2
+Loaded package environment from /home/armino112/.ghc/x86_64-linux-9.12.2/environments/default
+[1 of 2] Compiling Main             ( day03.hs, day03.o )
+[2 of 2] Linking day03
+Part 1: 17087
+Part 2: 169019504359949
+real    0m0.010s
+user    0m0.010s
+sys     0m0.000s
+
 ```
 
