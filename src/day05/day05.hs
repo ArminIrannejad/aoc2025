a31e0f9371547ff2e55572f63fbe2f8055bf7dae
ArminIrannejad <armin.irannejad@gmail.com>
2025-12-05 18:59:17 +0100

    chore: adds structure to day5

---
+++ src/day05/day05.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Text.Printf
+
+
+main :: IO ()
+main = do
+  cont <- lines <$> readFile "input.txt"
+  print cont
+  -- printf "Part 1 %d\n" $ part1 cont
+  -- printf "Part 2 %d\n" $ part2 cont
---
+++ src/day05/solution.md
@@ -0,0 +1,7 @@
+# [Day 5](https://adventofcode.com/2025/day/5) solution
+
+## Expected Result:
+
+```console
+
+```
