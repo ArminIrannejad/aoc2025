0786f5bd7c6addd9d33defc5af489ea8f7e7a22c
ArminIrannejad <armin.irannejad@gmail.com>
2025-12-05 17:01:27 +0100

    chore: adds solution to day 4 part 1

--- src/day04/day04.hs
+++ src/day04/day04.hs
@@ -1,8 +1,75 @@
 module Main where
 
+import Text.Printf
+
+type Input = [String]
+
+type Position = (Int, Int)
+
+type Direction = (Int, Int)
+
+directions :: [Direction]
+directions =
+  [ (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1)
+  ]
+
+height :: Input -> Int
+height = length
+
+width :: Input -> Int
+width [] = 0
+width (r : _) = length r
+
+inBounds :: Input -> Position -> Bool
+inBounds ls (r, c) =
+  r >= 0
+    && r < height ls
+    && c >= 0
+    && c < width ls
+
+lookupPos :: Input -> Position -> Maybe Char
+lookupPos ls p@(r, c)
+  | not (inBounds ls p) = Nothing
+  | otherwise = Just ((ls !! r) !! c)
+
+neighbors :: Input -> Position -> [Position]
+neighbors ls (r, c) =
+  [ (r + dr, c + dc)
+  | (dr, dc) <- directions,
+    let p = (r + dr, c + dc),
+    inBounds ls p
+  ]
+
+isAccessible :: Input -> Position -> Bool
+isAccessible ls p =
+  case lookupPos ls p of
+    Just '@' ->
+      let n =
+            length
+              [ ()
+              | np <- neighbors ls p,
+                lookupPos ls np == Just '@'
+              ]
+       in n < 4
+    _ -> False
+
+part1 :: Input -> Int
+part1 ls =
+  length
+    [ ()
+    | r <- [0 .. height ls - 1],
+      c <- [0 .. width ls - 1],
+      isAccessible ls (r, c)
+    ]
+
 main :: IO ()
 main = do
   cont <- lines <$> readFile "input.txt"
-  print cont
-  -- printf "Part 1: %d\n" $ part1 cont
-  -- printf "Part 2: %d\n" $ part2 cont
+  printf "Part 1 %d\n" $ part1 cont
